3f6e78b4c82c0d6c33e2d0f18ebb8ac0102ba622
youness <younessoudouh12@gmail.com>
2021-08-07 04:27:05 +0400

    reduce code

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ let cardContent = document.querySelector(".card-content");
 let btnSubmit = document.querySelector(".submit");
 let inputNote = document.querySelector(".note");
 let searchNote = document.querySelector(".search");
-let notesToDelet = Array.from(document.querySelectorAll(".new-note"))
 
 
 
-setInterval(SetUpDate, 1000);
+SetUpDate();
 btnSubmit.addEventListener("click", addNote);
 searchNote.addEventListener("keyup", filterNotes);
-cardContent.addEventListener("click", deletNote);
-inputNote.addEventListener("keyup", disabledBtn)
-document.addEventListener('DOMContentLoaded', getNotes) // get data from localStorage
+cardContent.addEventListener("click", checkNote)
+cardContent.addEventListener("click", deleteNote);
+inputNote.addEventListener("keyup", disabledBtn);
+document.addEventListener('DOMContentLoaded', getNotes); // get data from localStorage
 
 
 function disabledBtn() {
-    if (inputNote.value.length > 1) {
+    if (inputNote.value.length >= 1) {
         btnSubmit.disabled = false;
     } else {
         btnSubmit.disabled = true;
@@ -31,12 +31,12 @@ function SetUpDate() {
     console.log({ today: day })
     let year = date.getFullYear() + "";
     let yearLastTwoDigit = year.slice(2);
+    day = day < 10 ? "0" + day : day;
     divDate.textContent = day + "Th" + " " + month + "," + " " + yearLastTwoDigit;
 
 }
 
-function addNote() {
-
+function createElements(noteText) {
     var newDiv = document.createElement("div");
     let circleDiv = document.createElement("div");
     let newNote = document.createElement("h3");
@@ -45,15 +45,18 @@ function addNote() {
     circleDiv.classList.add("circle");
     newNote.classList.add("new-note-content");
 
-    newNote.textContent = inputNote.value;
-
-    // add note to localstorage
-
-    saveLocalNotes(newNote.textContent);
+    newNote.textContent = noteText;
 
     newDiv.appendChild(circleDiv);
     newDiv.appendChild(newNote);
     cardContent.insertBefore(newDiv, cardContent.firstElementChild);
+}
+
+function addNote() {
+    let inputValue = inputNote.value;
+    // add note to localstorage
+    saveLocalNotes(inputValue);
+    createElements(inputValue);
     inputNote.value = "";
     btnSubmit.disabled = true;
 }
@@ -76,21 +79,46 @@ function filterNotes(e) {
 
 }
 
-function deletNote(e) {
+function deleteNote(e) {
+
+    if (e.target.classList.contains("delet")) {
+
+
+        if (confirm("are you sure!!")) {
+            let textValue = e.target.previousElementSibling.textContent;
+            //remove from localStorage
+            removeLocalNotes(textValue);
+            e.target.parentElement.classList.add("fall");
+            document.querySelector(".fas").style.opacity = "1";
+            e.target.parentNode.addEventListener("transitionend", () => {
+                e.target.parentNode.remove();
+                document.querySelector(".fas").style.opacity = "0";
+            })
+        }
+
+
+    }
+
+}
+
+function checkNote(e) {
 
     if (e.target.classList.contains("circle")) {
 
         if (e.target.firstElementChild != null) {
-            if (confirm("are you sure!!")) {
-                let textValue = e.target.parentElement.lastElementChild.textContent;
-                //remove from localStorage
-                removeLocalNotes(textValue)
-                e.target.parentNode.remove()
-            }
+            e.target.firstElementChild.remove();
+            e.target.parentElement.lastElementChild.remove();
+
+
         } else {
             let tick = document.createElement("div");
+            let btn = document.createElement("button");
+            btn.textContent = "X";
+            btn.classList.add("delet")
+
             tick.classList.add("tick");
             e.target.appendChild(tick);
+            e.target.parentElement.appendChild(btn)
         }
 
     }
@@ -98,43 +126,32 @@ function deletNote(e) {
 }
 
 function saveLocalNotes(note) {
-    let notesSaved;
+    let notesSaved = checkLocalStr();
 
-    if (localStorage.getItem("notesSaved") === null) {
-        notesSaved = [];
-    } else {
-        notesSaved = JSON.parse(localStorage.getItem("notesSaved"));
-    }
     notesSaved.push(note);
-    localStorage.setItem("notesSaved", JSON.stringify(notesSaved))
+    localStorage.setItem("notesSaved", JSON.stringify(notesSaved));
 
 }
 
 function getNotes() {
-    let notesSaved;
-    if (localStorage.getItem("notesSaved") === null) {
-        notesSaved = [];
-    } else {
-        notesSaved = JSON.parse(localStorage.getItem("notesSaved"))
-    }
+    let notesSaved = checkLocalStr();
 
     notesSaved.forEach((note) => {
-        var newDiv = document.createElement("div");
-        let circleDiv = document.createElement("div");
-        let newNote = document.createElement("h3");
+        createElements(note);
 
-        newDiv.classList.add("new-note");
-        circleDiv.classList.add("circle");
-        newNote.classList.add("new-note-content");
-
-        newNote.textContent = note;
-        newDiv.appendChild(circleDiv);
-        newDiv.appendChild(newNote);
-        cardContent.insertBefore(newDiv, cardContent.firstElementChild);
-    })
+    });
 }
 
 function removeLocalNotes(note) {
+    let notesSaved = checkLocalStr();
+    //delet note from localstorage
+    let noteIndex = notesSaved.indexOf(note);
+
+    notesSaved.splice(noteIndex, 1);
+    localStorage.setItem("notesSaved", JSON.stringify(notesSaved));
+}
+
+function checkLocalStr() {
     let notesSaved;
 
     if (localStorage.getItem("notesSaved") === null) {
@@ -142,8 +159,6 @@ function removeLocalNotes(note) {
     } else {
         notesSaved = JSON.parse(localStorage.getItem("notesSaved"));
     }
-    let noteIndex = notesSaved.indexOf(note);
-    console.log(`index = ${noteIndex}`)
-    notesSaved.splice(noteIndex, 1);
-    localStorage.setItem("notesSaved", JSON.stringify(notesSaved))
+
+    return notesSaved;
 }
